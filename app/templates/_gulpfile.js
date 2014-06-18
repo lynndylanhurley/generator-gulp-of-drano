f0f8f990bbfde15d803f61fe2a4ca2fa72aaa232
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2014-06-18 10:33:55 -0500

    format interpolated project name strings

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -91,11 +91,11 @@ gulp.task('coffee', function() {
 // Images
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    //.pipe($.cache($.imagemin({
-      //optimizationLevel: 3,
-      //progressive: true,
-      //interlaced: true
-    //})))
+    .pipe($.cache($.imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
     .pipe(gulp.dest('.tmp/images'))
     .pipe($.size());
 });
